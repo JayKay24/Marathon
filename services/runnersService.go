88f81da368b87120540ced926f1dd216dc51083f
James Kinyua <jameskinyua590@gmail.com>
2023-08-13 23:47:45 +0300

    implement validateRunner function
    Signed-off-by: James Kinyua <jameskinyua590@gmail.com>

--- services/runnersService.go
+++ services/runnersService.go
@@ -1,6 +1,9 @@
 package services
 
-import "marathon-postgresql/models"
+import (
+	"marathon-postgresql/models"
+	"net/http"
+)
 
 type RunnersService struct {
 	runnersRepository *repositories.RunnersRepository
@@ -34,6 +37,36 @@ func (rs RunnersService) GetRunner(runnerId string) (*models.Runner, *models.Res
 func (rs RunnersService) GetRunnersBatch(country string, year string) ([]*models.Runner, *models.ResponseError) {
 }
 
-func validateRunner(runner *models.Runner) *models.ResponseError {}
+func validateRunner(runner *models.Runner) *models.ResponseError {
+	if runner.FirstName == "" {
+		return &models.ResponseError{
+			Message: "Invalid first name",
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	if runner.LastName == "" {
+		return &models.ResponseError{
+			Message: "Invalid last name",
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	if runner.Age <= 16 || runner.Age > 125 {
+		return &models.ResponseError{
+			Message: "Invalid age",
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	if runner.Country == "" {
+		return &models.ResponseError{
+			Message: "Invalid Country",
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	return nil
+}
 
 func validateRunnerId(runnerId string) *models.ResponseError {}
