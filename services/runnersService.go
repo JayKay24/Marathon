473371096c8017817a2b420d11f8332467920693
James Kinyua <jameskinyua590@gmail.com>
2023-08-14 20:10:53 +0300

    implement GetRunnersBatch service method
    Signed-off-by: James Kinyua <jameskinyua590@gmail.com>

--- services/runnersService.go
+++ services/runnersService.go
@@ -3,6 +3,8 @@ package services
 import (
 	"marathon-postgresql/models"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type RunnersService struct {
@@ -74,6 +76,38 @@ func (rs RunnersService) GetRunner(runnerId string) (*models.Runner, *models.Res
 }
 
 func (rs RunnersService) GetRunnersBatch(country string, year string) ([]*models.Runner, *models.ResponseError) {
+	if country != "" && year != "" {
+		return nil, &models.ResponseError{
+			Message: "Only one parameter can be passed",
+			Status:  http.StatusBadRequest,
+		}
+	}
+
+	if country != "" {
+		return rs.runnersRepository.GetRunnersByCountry(country)
+	}
+
+	if year != "" {
+		intYear, err := strconv.Atoi(year)
+		if err != nil {
+			return nil, &models.ResponseError{
+				Message: "Invalid year",
+				Status:  http.StatusBadRequest,
+			}
+		}
+
+		currentYear := time.Now().Year()
+		if intYear < 0 || intYear > currentYear {
+			return nil, &models.ResponseError{
+				Message: "Invalid Year",
+				Status:  http.StatusBadRequest,
+			}
+		}
+
+		return rs.runnersRepository.GetRunnersByYear(intYear)
+	}
+
+	return rs.runnersRepository.GetAllRunners()
 }
 
 func validateRunner(runner *models.Runner) *models.ResponseError {
