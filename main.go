de7144c133ef928b042d9cb47b93e1ab32c80216
James Kinyua <jameskinyua590@gmail.com>
2023-08-16 17:06:14 +0300

    implement InitDatabase function
    Signed-off-by: James Kinyua <jameskinyua590@gmail.com>

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"marathon-postgresql/config"
+	"marathon-postgresql/server"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	log.Println("Starting Runners App")
+	log.Println("Initializing configuration")
+	config := config.InitConfig("runners.toml")
+	log.Println("Initializing Database")
+	dbHandler := server.InitDatabase(config)
+	log.Println("Initializing HTTP Server")
+	httpServer := server.InitHttpServer(config, dbHandler)
+	httpServer.Start()
+}
